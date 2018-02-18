713e664854247229cd2aeb011a0777fef240a52a
Yoni <yoni.soleiman@gmail.com>
2018-02-18 19:29:46 +0100

    update

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import YHUIKit
 
 class ViewController: UIViewController {
 
-    let mainView: UIView = {
-        let mainView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 44.0, height: 44.0))
+    static let maxRandomViewSize = 60
+    static let minRandomViewSize = 10
+    
+    //MARK: - Main UI
+    
+    lazy var mainView: UIView = {
+        let mainView = UIView()
+        mainView.width = 44
+        mainView.height = mainView.width
         mainView.backgroundColor = UIColor.blue
+        mainView.layer.borderColor = UIColor.black.cgColor
+        mainView.layer.borderWidth = 1
         
         let panReco = UIPanGestureRecognizer(target: self, action: #selector(moveMainView))
         mainView.addGestureRecognizer(panReco)
@@ -20,15 +30,39 @@ class ViewController: UIViewController {
         return mainView
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    lazy var staticViews: [UIView] = {
+        var staticViews = [UIView]()
         
+        for _ in 0...10 {
+            staticViews.append(newRandomView())
+        }
+        
+        return staticViews
+    }()
+    
+    //MARK: - View Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        buildViewTree()
+        layout()
+    }
+    
+    func buildViewTree() {
+        
+        for staticView in staticViews {
+            view.addSubview(staticView)
+        }
         view.addSubview(mainView)
     }
 
+    func layout() {
+        mainView.centerInSuperview()
+    }
     
+    //MARK: - Gesture
     
-    @objc func moveMainView(panReco: UIPanGestureRecognizer) {
+    @objc func moveMainView(_ panReco: UIPanGestureRecognizer) {
         struct Static {
             static var delta: CGPoint = CGPoint.zero
         }
@@ -36,9 +70,29 @@ class ViewController: UIViewController {
         let location = panReco.location(in: view)
         
         if panReco.state == .began {
-            Static.delta = CGPoint(x: mainView.center.y, y: <#T##CGFloat#>)
+            Static.delta = CGPoint(x: mainView.centerX - location.x, y: mainView.centerY - location.y)
+        } else if panReco.state == .changed{
+            mainView.center = CGPoint(x: location.x + Static.delta.x, y: location.y + Static.delta.y)
         }
+    }
+    
+    //MARK: - Helper
+    
+    func newRandomView() -> UIView {
+        let randomView = UIView()
+        let randomWidth = Int(arc4random()) % (ViewController.maxRandomViewSize - ViewController.minRandomViewSize) + ViewController.minRandomViewSize
+        let randomHeight = Int(arc4random()) % (ViewController.maxRandomViewSize - ViewController.minRandomViewSize) + ViewController.minRandomViewSize
+        let randomHue = CGFloat( Int(arc4random()) % 360 ) / 360.0
         
+        randomView.width = CGFloat(randomWidth)
+        randomView.height = CGFloat(randomHeight)
+        
+        randomView.centerX = CGFloat(Int(arc4random()) % Int(view.bounds.width))
+        randomView.centerY = CGFloat(Int(arc4random()) % Int(view.bounds.height))
+        
+        randomView.backgroundColor = UIColor(hue: randomHue, saturation: 1, brightness: 1, alpha: 1)
+
+        return randomView
     }
 }
 
